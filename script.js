8d37363cf48799a9c7915bcc1310e761d5a465d7
sudhanshu mishra <sud.holics123@gmail.com>
2025-01-06 19:29:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let div = document.getElementById("move-div");
 let title = document.getElementById("title");
 
-//Detect touch device
 function isTouchDevice() {
   try {
-    //We try to create Touch Event (it would fail for desktops and throw error)
     document.createEvent("TouchEvent");
     return true;
   } catch (e) {
@@ -13,15 +11,11 @@ function isTouchDevice() {
 }
 
 const move = (e) => {
-  //Try catch to avoid any errors for touch screens(Error thrown when user doesn't move his finger)
   try {
-    /*
-    PageX and PageY return the position of clients cursor from the top left of screen
-    */
+  
     var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
     var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
   } catch (error) {}
-  //Set left and top of div based on mouse position
   div.style.left = x + "px";
   div.style.top = y + "px";
   const rect = title.getBoundingClientRect();
